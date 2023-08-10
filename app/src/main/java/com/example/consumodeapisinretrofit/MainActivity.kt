976ea4cb40dc84104c2e09f136655d3e379b441d
Jean <jepiosbu@gmail.com>
2023-08-10 16:00:13 -0500

    Porjecto terminado

--- app/src/main/java/com/example/consumodeapisinretrofit/MainActivity.kt
+++ app/src/main/java/com/example/consumodeapisinretrofit/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.consumodeapisinretrofit
 
+import android.app.DownloadManager.Request
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.consumodeapisinretrofit.databinding.ActivityMainBinding
+import org.json.JSONArray
+import org.json.JSONObject
 
 //¿Qué es una API?
 //Una API es el código que determina el funcionamiento de un programa informático que sirve para canalizar información de una parte de un software a otra.
@@ -23,10 +29,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setup()
+        sendJsonArray()
     }
 
-    open fun setup(){
+    open fun callstring(){
 
         //definimos la nueva instacia del requestQueue el cual es el objeto que se encarga de gestionar por automatico el envio de peticiones
         val queue = Volley.newRequestQueue(this)
@@ -42,5 +48,65 @@ class MainActivity : AppCompatActivity() {
 
         //añadimos al objeto request nuestra peticion de texto  y con esto ya tendriamos la llamada
         queue.add(stringRequest)
+    }
+    open fun callobject(){
+        //definimos la nueva instacia del requestQueue el cual es el objeto que se encarga de gestionar por automatico el envio de peticiones
+        val queue = Volley.newRequestQueue(this)
+        //direccion URL de la API que vamos a consumir
+        val url = "https://jsonplaceholder.typicode.com/"
+        //creamos un una peticion con la clase jsonObject request para hacer la llamada, cabe reclcar que si no recibe un Json object la llamada entrara en el error
+        val jsonObjectRequest = JsonObjectRequest(url,null, { response ->
+            binding.texto.text="Response is: ${response}" // nos devuelve el resultado obtenido del consumo o llamada a la api
+        },
+            {   error ->
+                binding.texto.text=error.printStackTrace().toString() //en caso contrario nos devuelve el error generado en caso de tener mal las dependencias
+            })                                                             //o los permisos de red
+
+        //añadimos al objeto request nuestra peticion de tipo jsonObject  y con esto ya tendriamos la llamada con un tipo objeto
+        queue.add(jsonObjectRequest)
+    }
+    open fun sendJsonbject(){
+        //empezamos instanciando el tipo de objeto que queremos enviar si cabe recalcar que el tipo de json que recibamos en un object debe ser object
+        //si es un json array debe ser array
+
+        //ejemplo json object
+        val jsonObject = JSONObject()
+        jsonObject.put("informacion enviada","true")
+
+        //definimos la nueva instacia del requestQueue el cual es el objeto que se encarga de gestionar por automatico el envio de peticiones
+        val queue = Volley.newRequestQueue(this)
+        //direccion URL de la API que vamos a consumir
+        val url = "https://jsonplaceholder.typicode.com/"
+        val jsonObjectRequest= JsonObjectRequest(url,/*aca pasamos el objeto a enviar*/jsonObject,{response ->
+                binding.texto.text="Response is:${response}"},
+            { error ->
+                binding.texto.text=error.printStackTrace().toString()
+            })
+        queue.add(jsonObjectRequest)
+    }
+    open fun sendJsonArray(){
+        //empezamos instanciando el tipo de objeto que queremos enviar si cabe recalcar que el tipo de json que recibamos en un object debe ser object
+        //si es un json array debe ser array
+
+        //ejemplo json Array
+        val jsonArray = JSONArray()
+        jsonArray.put(0)
+        jsonArray.put(1)
+        jsonArray.put(2)
+
+        //definimos la nueva instacia del requestQueue el cual es el objeto que se encarga de gestionar por automatico el envio de peticiones
+        val queue = Volley.newRequestQueue(this)
+        //direccion URL de la API que vamos a consumir
+        val url = "https://jsonplaceholder.typicode.com/"
+        val jsonArrayRequest= JsonArrayRequest(com.android.volley.Request.Method.POST,url,/*aca pasamos el Array a enviar*/jsonArray,{ response ->
+            binding.texto.text="Response is:${response}"},
+            { error ->
+                binding.texto.text="error"
+            })
+        queue.add(jsonArrayRequest)
+    }
+
+    fun send(cadena: String){
+        Toast.makeText(this, cadena, Toast.LENGTH_SHORT).show()
     }
 }
